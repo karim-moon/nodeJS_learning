e61bbc43178c7588b0d9e00ded2c529d8f76d8fb
moonjong <dksjfl@naver.com>
2019-10-24 22:05:12 +0900

    start!

--- learning javascript.js
+++ learning javascript.js
@@ -3,8 +3,8 @@ var result = 0;
 console.time('duration_sum');
 
 for (var i = 1; i <= 1000 ; i ++){
-    result += 1;
+    result += i;
 }
 
 console.timeEnd('duration_sum')
-console.log('1qnxj 1000까지 더한 결과물 : %d', result);
+console.log('1에서 1000까지 더한 결과물 : %d', result);
